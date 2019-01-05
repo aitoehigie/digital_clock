be018045ba635cae92325c0f4b9f128be70af7c0
aitoehigie <aitoehigie@gmail.com>
2019-01-05 22:42:42 +0100

    commit

--- digital_clock.py
+++ digital_clock.py
@@ -6,4 +6,12 @@ from tkiner import ttk
 import time
 import datetime
 
+def quit(*args):
+    root.destroy()
+
+def clock_time():
+    time = datetime.datetime.now()
+    time = (time.strftime("%H:%M:%S"))
+    txt.set(time)
+    root.after(1000, clock_time)
 
---
+++ introflask/intro/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello world</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
